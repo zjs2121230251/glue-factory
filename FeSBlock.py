b96f46c9f43ba207154eb163c5a8c058f3759bf7
zjs2121230251 <2121230251@qq.com>
2026-02-04 16:35:59 +0800

    added FeSB to extract FSM + VSSM + a

---
+++ FeSBlock.py
@@ -0,0 +1,360 @@
+import math
+import torch
+import torch.nn as nn
+import torch.utils.checkpoint as checkpoint
+import torch.nn.functional as F
+from functools import partial
+from typing import Optional, Callable
+from basicsr.utils.registry import ARCH_REGISTRY
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, selective_scan_ref
+from einops import rearrange, repeat
+from thop import profile
+from torch.nn import init as init
+
+# from model_archs.TTST_arc import Attention as TSA
+# from model_archs.layer import *
+# from model_archs.comm import *
+# from model_archs.uvmb import UVMB
+NEG_INF = -1000000
+
+class frequency_selection(nn.Module):
+    def __init__(self, dim, dw=1, norm='backward', act_method=nn.GELU, window_size=None, bias=False):
+        super(frequency_selection, self).__init__()
+        self.act_fft = act_method()
+        self.window_size = window_size
+        # dim = out_channel
+        hid_dim = dim * dw
+        # print(dim, hid_dim)
+        self.complex_weight1_real = nn.Parameter(torch.Tensor(dim, hid_dim))
+        self.complex_weight1_imag = nn.Parameter(torch.Tensor(dim, hid_dim))
+        self.complex_weight2_real = nn.Parameter(torch.Tensor(hid_dim, dim))
+        self.complex_weight2_imag = nn.Parameter(torch.Tensor(hid_dim, dim))
+        init.kaiming_uniform_(self.complex_weight1_real, a=math.sqrt(16))
+        init.kaiming_uniform_(self.complex_weight1_imag, a=math.sqrt(16))
+        init.kaiming_uniform_(self.complex_weight2_real, a=math.sqrt(16))
+        init.kaiming_uniform_(self.complex_weight2_imag, a=math.sqrt(16))
+        if bias:
+            self.b1_real = nn.Parameter(torch.zeros((1, 1, 1, hid_dim)), requires_grad=True)
+            self.b1_imag = nn.Parameter(torch.zeros((1, 1, 1, hid_dim)), requires_grad=True)
+            self.b2_real = nn.Parameter(torch.zeros((1, 1, 1, dim)), requires_grad=True)
+            self.b2_imag = nn.Parameter(torch.zeros((1, 1, 1, dim)), requires_grad=True)
+        self.bias = bias
+        self.norm = norm
+        # self.min = inf
+        # self.max = -inf
+
+    def forward(self, x):
+        _, _, H, W = x.shape
+        if self.window_size is not None and (H != self.window_size or W != self.window_size):
+            x, batch_list = window_partitionx(x, self.window_size)
+        y = torch.fft.rfft2(x, norm=self.norm)
+        dim = 1
+        weight1 = torch.complex(self.complex_weight1_real, self.complex_weight1_imag)
+        weight2 = torch.complex(self.complex_weight2_real, self.complex_weight2_imag)
+        if self.bias:
+            b1 = torch.complex(self.b1_real, self.b1_imag)
+            b2 = torch.complex(self.b2_real, self.b2_imag)
+        y = rearrange(y, 'b c h w -> b h w c')
+        y = y @ weight1
+        if self.bias:
+            y = y + b1
+        y = torch.cat([y.real, y.imag], dim=dim)
+
+        y = self.act_fft(y)
+        y_real, y_imag = torch.chunk(y, 2, dim=dim)
+        y = torch.complex(y_real, y_imag)
+        y = y @ weight2
+        if self.bias:
+            y = y + b2
+        y = rearrange(y, 'b h w c -> b c h w')
+        # y = torch.fft.irfft2(y, s=(H, W), norm=self.norm)
+        if self.window_size is not None and (H != self.window_size or W != self.window_size):
+            y = torch.fft.irfft2(y, s=(self.window_size, self.window_size), norm=self.norm)
+            y = window_reversex(y, self.window_size, H, W, batch_list)
+        else:
+            y = torch.fft.irfft2(y, s=(H, W), norm=self.norm)
+        return y
+
+class SS2D(nn.Module):
+    def __init__(
+            self,
+            d_model,
+            d_state=16,
+            d_conv=3,
+            expand=2.,
+            dt_rank="auto",
+            dt_min=0.001,
+            dt_max=0.1,
+            dt_init="random",
+            dt_scale=1.0,
+            dt_init_floor=1e-4,
+            dropout=0.,
+            conv_bias=True,
+            bias=False,
+            device=None,
+            dtype=None,
+            **kwargs,
+    ):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.d_model = d_model
+        self.d_state = d_state
+        self.d_conv = d_conv
+        self.expand = expand
+        self.d_inner = int(self.expand * self.d_model)
+        self.dt_rank = math.ceil(self.d_model / 16) if dt_rank == "auto" else dt_rank
+
+        self.in_proj = nn.Linear(self.d_model, self.d_inner * 2, bias=bias, **factory_kwargs)
+        self.conv2d = nn.Conv2d(
+            in_channels=self.d_inner,
+            out_channels=self.d_inner,
+            groups=self.d_inner,
+            bias=conv_bias,
+            kernel_size=d_conv,
+            padding=(d_conv - 1) // 2,
+            **factory_kwargs,
+        )
+        self.act = nn.SiLU()
+
+        self.x_proj = (
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+            nn.Linear(self.d_inner, (self.dt_rank + self.d_state * 2), bias=False, **factory_kwargs),
+        )
+        self.x_proj_weight = nn.Parameter(torch.stack([t.weight for t in self.x_proj], dim=0))  # (K=4, N, inner)
+        del self.x_proj
+
+        self.dt_projs = (
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+            self.dt_init(self.dt_rank, self.d_inner, dt_scale, dt_init, dt_min, dt_max, dt_init_floor,
+                         **factory_kwargs),
+        )
+        self.dt_projs_weight = nn.Parameter(torch.stack([t.weight for t in self.dt_projs], dim=0))  # (K=4, inner, rank)
+        self.dt_projs_bias = nn.Parameter(torch.stack([t.bias for t in self.dt_projs], dim=0))  # (K=4, inner)
+        del self.dt_projs
+
+        self.A_logs = self.A_log_init(self.d_state, self.d_inner, copies=4, merge=True)  # (K=4, D, N)
+        self.Ds = self.D_init(self.d_inner, copies=4, merge=True)  # (K=4, D, N)
+
+        self.selective_scan = selective_scan_fn
+
+        self.out_norm = nn.LayerNorm(self.d_inner)
+        self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=bias, **factory_kwargs)
+        self.dropout = nn.Dropout(dropout) if dropout > 0. else None
+
+    @staticmethod
+    def dt_init(dt_rank, d_inner, dt_scale=1.0, dt_init="random", dt_min=0.001, dt_max=0.1, dt_init_floor=1e-4,
+                **factory_kwargs):
+        dt_proj = nn.Linear(dt_rank, d_inner, bias=True, **factory_kwargs)
+
+        # Initialize special dt projection to preserve variance at initialization
+        dt_init_std = dt_rank ** -0.5 * dt_scale
+        if dt_init == "constant":
+            nn.init.constant_(dt_proj.weight, dt_init_std)
+        elif dt_init == "random":
+            nn.init.uniform_(dt_proj.weight, -dt_init_std, dt_init_std)
+        else:
+            raise NotImplementedError
+
+        # Initialize dt bias so that F.softplus(dt_bias) is between dt_min and dt_max
+        dt = torch.exp(
+            torch.rand(d_inner, **factory_kwargs) * (math.log(dt_max) - math.log(dt_min))
+            + math.log(dt_min)
+        ).clamp(min=dt_init_floor)
+        # Inverse of softplus: https://github.com/pytorch/pytorch/issues/72759
+        inv_dt = dt + torch.log(-torch.expm1(-dt))
+        with torch.no_grad():
+            dt_proj.bias.copy_(inv_dt)
+        # Our initialization would set all Linear.bias to zero, need to mark this one as _no_reinit
+        dt_proj.bias._no_reinit = True
+
+        return dt_proj
+
+    @staticmethod
+    def A_log_init(d_state, d_inner, copies=1, device=None, merge=True):
+        # S4D real initialization
+        A = repeat(
+            torch.arange(1, d_state + 1, dtype=torch.float32, device=device),
+            "n -> d n",
+            d=d_inner,
+        ).contiguous()
+        A_log = torch.log(A)  # Keep A_log in fp32
+        if copies > 1:
+            A_log = repeat(A_log, "d n -> r d n", r=copies)
+            if merge:
+                A_log = A_log.flatten(0, 1)
+        A_log = nn.Parameter(A_log)
+        A_log._no_weight_decay = True
+        return A_log
+
+    @staticmethod
+    def D_init(d_inner, copies=1, device=None, merge=True):
+        # D "skip" parameter
+        D = torch.ones(d_inner, device=device)
+        if copies > 1:
+            D = repeat(D, "n1 -> r n1", r=copies)
+            if merge:
+                D = D.flatten(0, 1)
+        D = nn.Parameter(D)  # Keep in fp32
+        D._no_weight_decay = True
+        return D
+
+    def forward_core(self, x: torch.Tensor):
+        B, C, H, W = x.shape
+        L = H * W
+        K = 4
+        x_hwwh = torch.stack([x.view(B, -1, L), torch.transpose(x, dim0=2, dim1=3).contiguous().view(B, -1, L)], dim=1).view(B, 2, -1, L)
+        xs = torch.cat([x_hwwh, torch.flip(x_hwwh, dims=[-1])], dim=1) # (1, 4, 192, 3136)
+
+        x_dbl = torch.einsum("b k d l, k c d -> b k c l", xs.view(B, K, -1, L), self.x_proj_weight)
+        dts, Bs, Cs = torch.split(x_dbl, [self.dt_rank, self.d_state, self.d_state], dim=2)
+        dts = torch.einsum("b k r l, k d r -> b k d l", dts.view(B, K, -1, L), self.dt_projs_weight)
+        xs = xs.float().view(B, -1, L)
+        dts = dts.contiguous().float().view(B, -1, L) # (b, k * d, l)
+        Bs = Bs.float().view(B, K, -1, L)
+        Cs = Cs.float().view(B, K, -1, L) # (b, k, d_state, l)
+        Ds = self.Ds.float().view(-1)
+        As = -torch.exp(self.A_logs.float()).view(-1, self.d_state)
+        dt_projs_bias = self.dt_projs_bias.float().view(-1) # (k * d)
+        out_y = self.selective_scan(
+            xs, dts,
+            As, Bs, Cs, Ds, z=None,
+            delta_bias=dt_projs_bias,
+            delta_softplus=True,
+            return_last_state=False,
+        ).view(B, K, -1, L)
+        assert out_y.dtype == torch.float
+
+        inv_y = torch.flip(out_y[:, 2:4], dims=[-1]).view(B, 2, -1, L)
+        wh_y = torch.transpose(out_y[:, 1].view(B, -1, W, H), dim0=2, dim1=3).contiguous().view(B, -1, L)
+        invwh_y = torch.transpose(inv_y[:, 1].view(B, -1, W, H), dim0=2, dim1=3).contiguous().view(B, -1, L)
+
+        return out_y[:, 0], inv_y[:, 0], wh_y, invwh_y
+
+    def forward(self, x: torch.Tensor, **kwargs):
+        B, H, W, C = x.shape
+
+        xz = self.in_proj(x)
+        x, z = xz.chunk(2, dim=-1)
+
+        x = x.permute(0, 3, 1, 2).contiguous()
+        x = self.act(self.conv2d(x))
+        y1, y2, y3, y4 = self.forward_core(x)
+        assert y1.dtype == torch.float32
+        y = y1 + y2 + y3 + y4
+        y = torch.transpose(y, dim0=1, dim1=2).contiguous().view(B, H, W, -1)
+        y = self.out_norm(y)
+        y = y * F.silu(z)
+        out = self.out_proj(y)
+        if self.dropout is not None:
+            out = self.dropout(out)
+        return out
+
+
+class VSSBlock(nn.Module):
+    def __init__(
+            self,
+            hidden_dim: int = 0,
+            drop_path: float = 0,
+            norm_layer: Callable[..., torch.nn.Module] = partial(nn.LayerNorm, eps=1e-6),
+            attn_drop_rate: float = 0,
+            d_state: int = 16,
+            expand: float = 2.,
+            is_light_sr: bool = False,
+            **kwargs,
+    ):
+        super().__init__()
+        self.ln_1 = norm_layer(hidden_dim)
+        self.self_attention = SS2D(d_model=hidden_dim, d_state=d_state, expand=expand,dropout=attn_drop_rate, **kwargs)
+        self.fft_branch0 = frequency_selection(dim=hidden_dim)
+
+        # self.self_attention = UV
+
+        self.drop_path = DropPath(drop_path)
+        self.skip_scale= nn.Parameter(torch.ones(hidden_dim))
+
+        #self.hybridgate = HybridGate(dim=hidden_dim, mlp_ratio=2.)
+        self.fft_branch = frequency_selection(dim=hidden_dim)
+        # self.frequencyCA = MultiSpectralAttentionLayer(channel=hidden_dim, dct_h=21, dct_w=21)
+        self.ln_2 = nn.LayerNorm(hidden_dim)
+        self.skip_scale2 = nn.Parameter(torch.ones(hidden_dim))
+
+
+
+    def forward(self, input, x_size):
+        # x [B,HW,C]
+        B, L, C = input.shape
+        input = input.view(B, *x_size, C).contiguous()  # [B,H,W,C]
+        fft0 = self.fft_branch0(input.permute(0, 3, 1, 2).contiguous())#G_FSM
+        fft0 = fft0.permute(0, 2, 3, 1).contiguous()
+
+        x = self.ln_1(input)  # B H W C
+        x = input*self.skip_scale + fft0 + self.drop_path(self.self_attention(x))#全局模块输出y
+        # x = input*self.skip_scale + self.drop_path(self.self_attention(x))
+        # print(000, x.size())
+        fft = x.permute(0, 3, 1, 2).contiguous()
+        fft = self.fft_branch(fft)
+        fft = fft.permute(0, 2, 3, 1).contiguous()
+        # x = x*self.skip_scale2 + self.conv_blk(self.ln_2(x).permute(0, 3, 1, 2).contiguous()).permute(0, 2, 3, 1).contiguous()
+        x = x*self.skip_scale2 + fft + self.hybridgate(self.ln_2(x).permute(0, 3, 1, 2).contiguous()).permute(0, 2, 3, 1).contiguous()  # B H W C
+
+        x = x.view(B, -1, C).contiguous()
+        return x
+    
+
+class VSSBlock(nn.Module):
+    def __init__(
+            self,
+            hidden_dim: int = 0,
+            drop_path: float = 0,
+            norm_layer: Callable[..., torch.nn.Module] = partial(nn.LayerNorm, eps=1e-6),
+            attn_drop_rate: float = 0,
+            d_state: int = 16,
+            expand: float = 2.,
+            is_light_sr: bool = False,
+            **kwargs,
+    ):
+        super().__init__()
+        self.ln_1 = norm_layer(hidden_dim)
+        self.self_attention = SS2D(d_model=hidden_dim, d_state=d_state, expand=expand,dropout=attn_drop_rate, **kwargs)
+        self.fft_branch0 = frequency_selection(dim=hidden_dim)
+
+        # self.self_attention = UV
+
+        self.drop_path = DropPath(drop_path)
+        self.skip_scale= nn.Parameter(torch.ones(hidden_dim))
+
+        self.hybridgate = HybridGate(dim=hidden_dim, mlp_ratio=2.)
+        self.fft_branch = frequency_selection(dim=hidden_dim)
+        self.frequencyCA = MultiSpectralAttentionLayer(channel=hidden_dim, dct_h=21, dct_w=21)
+        self.ln_2 = nn.LayerNorm(hidden_dim)
+        self.skip_scale2 = nn.Parameter(torch.ones(hidden_dim))
+
+
+
+    def forward(self, input, x_size):
+        # x [B,HW,C]
+        B, L, C = input.shape
+        input = input.view(B, *x_size, C).contiguous()  # [B,H,W,C]
+        fft0 = self.fft_branch0(input.permute(0, 3, 1, 2).contiguous())#G_FSM
+        fft0 = fft0.permute(0, 2, 3, 1).contiguous()
+
+        x = self.ln_1(input)  # B H W C
+        x = input*self.skip_scale + fft0 + self.drop_path(self.self_attention(x))#全局模块输出y
+        # x = input*self.skip_scale + self.drop_path(self.self_attention(x))
+        # print(000, x.size())
+        fft = x.permute(0, 3, 1, 2).contiguous()
+        fft = self.fft_branch(fft)
+        fft = fft.permute(0, 2, 3, 1).contiguous()
+        # x = x*self.skip_scale2 + self.conv_blk(self.ln_2(x).permute(0, 3, 1, 2).contiguous()).permute(0, 2, 3, 1).contiguous()
+        x = x*self.skip_scale2 + fft + self.hybridgate(self.ln_2(x).permute(0, 3, 1, 2).contiguous()).permute(0, 2, 3, 1).contiguous()  # B H W C
+
+        x = x.view(B, -1, C).contiguous()
+        return x
